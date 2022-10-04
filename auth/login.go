d419ee83140525f4aa7eea280cb681d630f92add
harshmangalam <harshdev8218@gmail.com>
2022-10-04 09:19:07 +0530

    updated login

---
+++ auth/login.go
@@ -0,0 +1,24 @@
+package auth
+
+type LoginSchema struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func NewLogin() *LoginSchema {
+	return new(LoginSchema)
+}
+
+func (l *LoginSchema) SetEmail(email string) *LoginSchema {
+	l.Email = email
+	return l
+}
+
+func (l *LoginSchema) SetPassword(password string) *LoginSchema {
+	l.Email = password
+	return l
+}
+
+func (l *LoginSchema) Send() any {
+	return "authenticate..."
+}
