91b4b4f0deec0fd4e3e50d8229e99a10da7056f7
harshmangalam <harshdev8218@gmail.com>
2022-10-04 08:44:49 +0530

    added login methods

--- auth/login.go
+++ auth/login.go
@@ -4,3 +4,17 @@ type LoginSchema struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+func NewLogin() *LoginSchema {
+	return new(LoginSchema)
+}
+
+func (l *LoginSchema) SetEmail(email string) *LoginSchema {
+	l.Email = email
+	return l
+}
+
+func (l *LoginSchema) SetPassword(password string) *LoginSchema {
+	l.Email = password
+	return l
+}
