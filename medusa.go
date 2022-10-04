f3741e31a3dc3eb3f8f46f30644be34d8a8a9380
harshmangalam <harshdev8218@gmail.com>
2022-10-04 09:03:32 +0530

    added medusa type

---
+++ medusa.go
@@ -0,0 +1,8 @@
+package medusasdkgolang
+
+type Medusa struct {
+	MaxRetries int8   // The amount of times a request is retried.
+	BaseUrl    string // The url to which requests are made to.
+	ApiKey     string //Optional api key used for authenticating admin requests .
+
+}
