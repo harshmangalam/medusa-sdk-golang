84f22bf582c7c90e64c71bc79e7a09f5738e19aa
harshmangalam <harshdev8218@gmail.com>
2022-10-04 12:50:49 +0530

    updated config and apikey

--- medusa.go
+++ medusa.go
@@ -1,32 +1,30 @@
 package medusasdkgolang
 
-import "net/http"
-
-type Medusa struct {
-	MaxRetries int8         // The amount of times a request is retried.
-	BaseUrl    string       // The url to which requests are made to.
-	Cookie     *http.Cookie //cookie connect.sid for authenticated request
+type Config struct {
+	MaxRetries int8   // The amount of times a request is retried.
+	BaseUrl    string // The url to which requests are made to.
+	ApiKey     string // For request on authorized routes
 }
 
-func NewMedusa() *Medusa {
-	m := new(Medusa)
+func NewConfig() *Config {
+	m := new(Config)
 	// add default value
 	m.MaxRetries = 0
 	m.BaseUrl = "http://localhost:9000"
 	return m
 }
 
-func (m *Medusa) SetMaxRetries(maxRetries int8) *Medusa {
+func (m *Config) SetMaxRetries(maxRetries int8) *Config {
 	m.MaxRetries = maxRetries
 	return m
 }
 
-func (m *Medusa) SetBaseUrl(baseUrl string) *Medusa {
+func (m *Config) SetBaseUrl(baseUrl string) *Config {
 	m.BaseUrl = baseUrl
 	return m
 }
 
-func (m *Medusa) SetCookie(cookie *http.Cookie) *Medusa {
-	m.Cookie = cookie
+func (m *Config) SetApiKey(apiKey string) *Config {
+	m.ApiKey = apiKey
 	return m
 }
