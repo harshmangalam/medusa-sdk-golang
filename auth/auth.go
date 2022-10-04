9c79719ab46f19d606250e96250e3e223634ec07
harshmangalam <harshdev8218@gmail.com>
2022-10-04 12:22:37 +0530

    updated authhenticate

--- auth/auth.go
+++ auth/auth.go
@@ -5,6 +5,7 @@ import (
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
 type AuthSchema struct {
@@ -29,12 +30,28 @@ func (l *AuthSchema) SetPassword(password string) *AuthSchema {
 func (l *AuthSchema) Authenticate(medusa *medusa.Medusa) ([]byte, error) {
 	path := "/store/auth"
 
-	res, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(l).Send(medusa)
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(l).Send(medusa)
 
 	if err != nil {
 		return nil, err
 	}
-	return res, nil
+
+	body, err := utils.ParseResponseBody(resp)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode == http.StatusOK {
+
+		for _, cookie := range resp.Cookies() {
+			if cookie.Name == "connect.sid" {
+				medusa.SetCookie(cookie)
+			}
+
+		}
+	}
+	return body, nil
 }
 
 func GetSession(m *medusa.Medusa) ([]byte, error) {
