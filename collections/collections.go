ff695ab94817a9445fc38121de5454a4cae98d4f
harshmangalam <harshdev8218@gmail.com>
2022-10-05 09:39:45 +0530

    updated comment doc

--- collections/collections.go
+++ collections/collections.go
@@ -17,6 +17,42 @@ type CollectionsQuery struct {
 	UpdatedAt *common.DateComparisonOperator `json:"updated_at,omitempty"`
 }
 
+// create new collections query
+func NewCollectionsQuery() *CollectionsQuery {
+	c := new(CollectionsQuery)
+	c.Limit = 10
+	c.Offset = 0
+
+	return c
+}
+
+// The number of collections to return
+func (c *CollectionsQuery) SetLimit(limit int) *CollectionsQuery {
+	c.Limit = limit
+	return c
+}
+
+// The number of collections to skip before starting to collect the collections set
+func (c *CollectionsQuery) SetOffset(offset int) *CollectionsQuery {
+	c.Offset = offset
+	return c
+}
+
+// Retrieve a list of Product Collection.
+func (c *CollectionsQuery) List(config *medusa.Config) ([]byte, error) {
+	path := "/store/collections"
+
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
 // Retrieves a Product Collection.
 
 func Retrieve(id string, config *medusa.Config) ([]byte, error) {
@@ -31,39 +67,4 @@ func Retrieve(id string, config *medusa.Config) ([]byte, error) {
 	}
 	return body, nil
 
-}
-
-// Retrieve a list of Product Collection.
-
-func NewCollectionsQuery() *CollectionsQuery {
-	c := new(CollectionsQuery)
-	c.Limit = 10
-	c.Offset = 0
-
-	return c
-}
-
-func (c *CollectionsQuery) SetLimit(limit int) *CollectionsQuery {
-	c.Limit = limit
-	return c
-}
-
-// The number of collections to skip before starting to collect the collections set
-func (c *CollectionsQuery) SetOffset(offset int) *CollectionsQuery {
-	c.Offset = offset
-	return c
-}
-
-func (c *CollectionsQuery) List(config *medusa.Config) ([]byte, error) {
-	path := "/store/collections"
-
-	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
-	if err != nil {
-		return nil, err
-	}
-	body, err := utils.ParseResponseBody(resp)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
 }
