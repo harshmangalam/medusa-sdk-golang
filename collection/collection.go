6663633740242e03a47fcbee1033d75e49c17f60
harshmangalam <harshdev8218@gmail.com>
2022-10-05 08:34:53 +0530

    retrieve collection by id

---
+++ collection/collection.go
@@ -0,0 +1,24 @@
+package collection
+
+import (
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+func Retrieve(id string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/collections/%v", id)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+
+}
