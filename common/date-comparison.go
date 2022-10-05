100929bdbdaf40a8f1953dc74e180710d674d44f
harshmangalam <harshdev8218@gmail.com>
2022-10-05 09:51:41 +0530

    added date comparison

---
+++ common/date-comparison.go
@@ -0,0 +1,10 @@
+package common
+
+import "time"
+
+type DateComparison struct {
+	Lt  time.Time `json:"lt,omitempty"`
+	Gt  time.Time `json:"gt,omitempty"`
+	Lte time.Time `json:"lte,omitempty"`
+	Gte time.Time `json:"gte,omitempty"`
+}
