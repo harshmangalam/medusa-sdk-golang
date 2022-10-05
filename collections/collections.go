d7acdd27e97bbff6d30c8b0e6b5f054b269fe940
harshmangalam <harshdev8218@gmail.com>
2022-10-05 10:18:10 +0530

    added collection struct doc

--- collections/collections.go
+++ collections/collections.go
@@ -12,15 +12,31 @@ import (
 )
 
 type Collection struct {
-	Title     string    `json:"title"`
-	Id        string    `json:"id,omitempty"`
-	Handle    string    `json:"handle,omitempty"`
-	Products  []any     `json:"products,omitempty"`
+	// The title that the Product Collection is identified by.
+	Title string `json:"title"`
+
+	// The product collection's ID
+	Id string `json:"id,omitempty"`
+
+	// A unique string that identifies the Product Collection - can for example be used in slug structures.
+	Handle string `json:"handle,omitempty"`
+
+	// The Products contained in the Product Collection. Available if the relation products is expanded.
+	Products []any `json:"products,omitempty"`
+
+	// The date with timezone at which the resource was created.
 	CreatedAt time.Time `json:"created_at,omitempty"`
+
+	// The date with timezone at which the resource was updated.
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
+
+	// The date with timezone at which the resource was deleted.
 	DeletedAt time.Time `json:"deleted_at,omitempty"`
-	Metadata  any       `json:"metadata,omitempty"`
+
+	// An optional key-value map with additional details
+	Metadata any `json:"metadata,omitempty"`
 }
+
 type CollectionsQuery struct {
 	Limit     int                    `json:"limit,omitempty"`
 	Offset    int                    `json:"offset,omitempty"`
