d4e6e754f5421446fdae7fbc3f6a0a5a1ce4eabb
harshmangalam <harshdev8218@gmail.com>
2022-10-05 09:55:07 +0530

    add date filter

--- common/date-comparison.go
+++ common/date-comparison.go
@@ -9,6 +9,13 @@ type DateComparison struct {
 	Gte time.Time `json:"gte,omitempty"`
 }
 
+// create new date comparison
 func NewDateComparison() *DateComparison {
 	return new(DateComparison)
+}
+
+// filter by dates less than this date
+func (d *DateComparison) SetLt(lt time.Time) *DateComparison {
+	d.Lt = lt
+	return d
 }
