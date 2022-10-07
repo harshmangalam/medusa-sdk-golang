b6f71ed11212b9a0000131604c5540fb6a3e2964
harshmangalam <harshdev8218@gmail.com>
2022-10-07 09:43:52 +0530

    added search query methods

--- products/search.go
+++ products/search.go
@@ -5,3 +5,22 @@ type SearchQuery struct {
 	Offset int    `json:"offset" url:"offset"`
 	Limit  int    `json:"limit" url:"limit"`
 }
+
+func NewSearchQuery() *SearchQuery {
+	return new(SearchQuery)
+}
+
+func (s *SearchQuery) SetQ(q string) *SearchQuery {
+	s.Q = q
+	return s
+}
+
+func (s *SearchQuery) SetOffset(offset int) *SearchQuery {
+	s.Offset = offset
+	return s
+}
+
+func (s *SearchQuery) SetLimit(limit int) *SearchQuery {
+	s.Limit = limit
+	return s
+}
