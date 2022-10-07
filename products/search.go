2e1c2a26472b158c2bb923629cd45358d582eacc
harshmangalam <harshdev8218@gmail.com>
2022-10-07 10:20:23 +0530

    search products

--- products/search.go
+++ products/search.go
@@ -1,9 +1,19 @@
 package products
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/google/go-querystring/query"
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
 type SearchQuery struct {
-	Q      string `json:"q" url:"q"`
-	Offset int    `json:"offset" url:"offset"`
-	Limit  int    `json:"limit" url:"limit"`
+	Q      string `json:"q,omitempty" url:"q,omitempty"`
+	Offset int    `json:"offset,omitempty" url:"offset,omitempty"`
+	Limit  int    `json:"limit,omitempty" url:"limit,omitempty"`
 }
 
 func NewSearchQuery() *SearchQuery {
@@ -23,4 +33,33 @@ func (s *SearchQuery) SetOffset(offset int) *SearchQuery {
 func (s *SearchQuery) SetLimit(limit int) *SearchQuery {
 	s.Limit = limit
 	return s
+}
+
+func (s *SearchQuery) Search(config *medusa.Config) ([]byte, error) {
+	path := "/store/products/search"
+
+	qs, err := query.Values(s)
+	if err != nil {
+		return nil, err
+	}
+
+	parseStr := qs.Encode()
+
+	path = fmt.Sprintf("%v?%v", path, parseStr)
+
+	fmt.Println(path)
+	resp, err := request.
+		NewRequest().
+		SetMethod(http.MethodPost).
+		SetPath(path).
+		Send(config)
+
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
