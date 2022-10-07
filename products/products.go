e901f4bbb0097902d1da80aeecad96f1b7bda790
harshmangalam <harshdev8218@gmail.com>
2022-10-07 11:48:42 +0530

    added product options

--- products/products.go
+++ products/products.go
@@ -1,5 +1,7 @@
 package products
 
+import "github.com/harshmngalam/medusa-sdk-golang/common"
+
 type ProductStatus string
 
 const (
@@ -9,24 +11,36 @@ const (
 	Rejected  ProductStatus = "rejected"
 )
 
+type ProductOptions struct {
+	Title     string         `json:"title"`
+	ProductId string         `json:"product_id"`
+	Id        string         `json:"id,omitempty"`
+	Values    []any          `json:"values"`
+	Product   *Product       `json:"product"`
+	CreatedAt string         `json:"created_at,omitempty"`
+	UpdatedAt string         `json:"updated_at,omitempty"`
+	DeletedAt string         `json:"deleted_at,omitempty"`
+	Metadata  map[string]any `json:"metadata,omitempty"`
+}
+
 type Product struct {
-	Title         string        `json:"title"`
-	ProfileId     string        `json:"profile_id"`
-	Id            string        `json:"id"`
-	Subtitle      string        `json:"subtitle"`
-	Description   string        `json:"description"`
-	Handle        string        `json:"handle"`
-	IsGiftcard    bool          `json:"is_giftcard"`
-	Status        ProductStatus `json:"status"`
-	Images        []Image       `json:"images"`
-	Thumbnail     string        `json:"thumbnail"`
-	Options       []Option      `json:"options"`
-	Variants      []Variant     `json:"variants"`
-	Profile       Profile       `json:"profile"`
-	Weight        int           `json:"weight"`
-	Height        int           `json:"height"`
-	Width         int           `json:"width"`
-	Length        int           `json:"length"`
+	Title         string            `json:"title"`
+	ProfileId     string            `json:"profile_id"`
+	Id            string            `json:"id"`
+	Subtitle      string            `json:"subtitle"`
+	Description   string            `json:"description"`
+	Handle        string            `json:"handle"`
+	IsGiftcard    bool              `json:"is_giftcard"`
+	Status        ProductStatus     `json:"status"`
+	Images        []*common.Image   `json:"images"`
+	Thumbnail     string            `json:"thumbnail"`
+	Options       []*ProductOptions `json:"options"`
+	Variants      []Variant         `json:"variants"`
+	Profile       Profile           `json:"profile"`
+	Weight        int               `json:"weight"`
+	Height        int               `json:"height"`
+	Width         int               `json:"width"`
+	Length        int               `json:"length"`
 	HsCode        string
 	OriginCountry string         `json:"origin_country"`
 	MidCode       string         `json:"mid_code"`
