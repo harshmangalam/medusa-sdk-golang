74d953d704e9d110106d46c0be8ee3e8fdfd88b3
harshmangalam <harshdev8218@gmail.com>
2022-10-10 13:45:37 +0530

    retrieve collection

---
+++ collections/retrieve.go
@@ -0,0 +1,26 @@
+package collections
+
+import (
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+// Retrieves a Product Collection.
+
+func Retrieve(id string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/collections/%v", id)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+
+}
