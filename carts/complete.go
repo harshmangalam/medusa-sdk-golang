2762f28008c2dddb52199ccb67633abdb1050145
harshmangalam <harshdev8218@gmail.com>
2022-10-10 18:33:10 +0530

    added cart complete

---
+++ carts/complete.go
@@ -0,0 +1,23 @@
+package carts
+
+import (
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+func Complete(cart_id string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/carts/%v/complete", cart_id)
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
