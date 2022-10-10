237a6d28a7d505c48771f64bff7f13f62903a169
harshmangalam <harshdev8218@gmail.com>
2022-10-10 18:44:48 +0530

    create cart

--- carts/create.go
+++ carts/create.go
@@ -1,13 +1,34 @@
 package carts
 
+import (
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
 type CartItem struct {
 	VariantId string `json:"variant_id"`
 	Quantity  int    `json:"quantity"`
 }
-type CreateCartSchema struct {
+type CreateCart struct {
 	RegionId       string         `json:"region_id"`
 	SalesChannelId string         `json:"sales_channel_id"`
 	CountryCode    string         `json:"country_code"`
 	Items          []*CartItem    `json:"items"`
 	Context        map[string]any `json:"context"`
+}
+
+func (c *CreateCart) Create(config *medusa.Config) ([]byte, error) {
+	const path = `/store/carts`
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetData(c).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
