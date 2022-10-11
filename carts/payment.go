f7faf230e2ddcbbb19139ba2d5b4e0c435a5df9d
harshmangalam <harshdev8218@gmail.com>
2022-10-11 19:39:23 +0530

    create payment session

---
+++ carts/payment.go
@@ -0,0 +1,23 @@
+package carts
+
+import (
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+func CreatePaymentSession(cartId string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/carts/%v/payment-sessions", cartId)
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
