55a8e08f5cbc78ce31a8950df36c508bd5f96d26
harshmangalam <harshdev8218@gmail.com>
2022-10-12 10:42:08 +0530

    update cart schema

--- carts/update.go
+++ carts/update.go
@@ -1,14 +1,14 @@
 package carts
 
 type UpdateCart struct {
-	RegionId         string         `json:"region_id"`
-	country_code     string         `json:"country_code"`
-	email            string         `json:"email"`
-	sales_channel_id string         `json:"sales_channel_id"`
-	billing_address  any            `json:"billing_address"`
-	ipping_address   any            `json:"ipping_address"`
-	gift_cards       any            `json:"gift_cards"`
-	discounts        any            `json:"discounts"`
-	customer_id      string         `json:"customer_id"`
-	Context          map[string]any `json:"context"`
+	RegionId       string         `json:"region_id"`
+	CountryCode    string         `json:"country_code"`
+	Email          string         `json:"email"`
+	SalesChannelId string         `json:"sales_channel_id"`
+	BillingAddress any            `json:"billing_address"`
+	SippingAddress any            `json:"ipping_address"`
+	GiftCards      any            `json:"gift_cards"`
+	Discounts      any            `json:"discounts"`
+	CustomerId     string         `json:"customer_id"`
+	Context        map[string]any `json:"context"`
 }
