006e57cfbb077395808b255f6985d9fa1eded0ba
harshmangalam <harshdev8218@gmail.com>
2022-10-12 10:44:14 +0530

    added gift card

--- carts/update.go
+++ carts/update.go
@@ -3,6 +3,10 @@ package carts
 type Discount struct {
 	Code string `json:"code"`
 }
+
+type GiftCard struct {
+	Code string `json:"code"`
+}
 type UpdateCart struct {
 	RegionId       string         `json:"region_id"`
 	CountryCode    string         `json:"country_code"`
@@ -10,7 +14,7 @@ type UpdateCart struct {
 	SalesChannelId string         `json:"sales_channel_id"`
 	BillingAddress any            `json:"billing_address"`
 	SippingAddress any            `json:"ipping_address"`
-	GiftCards      any            `json:"gift_cards"`
+	GiftCards      []*GiftCard    `json:"gift_cards"`
 	Discounts      []*Discount    `json:"discounts"`
 	CustomerId     string         `json:"customer_id"`
 	Context        map[string]any `json:"context"`
