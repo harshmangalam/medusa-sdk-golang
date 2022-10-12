4238adfac8d2f51b7b8ac3bb9a3a99028f31368e
harshmangalam <harshdev8218@gmail.com>
2022-10-12 11:22:24 +0530

    added create customer schema

---
+++ customers/create.go
@@ -0,0 +1,9 @@
+package customers
+
+type CreateCustomer struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Phone     string `json:"phone"`
+}
