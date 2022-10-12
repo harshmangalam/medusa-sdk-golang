4083540a3fee41c054572e3b91e2d13c93dbcb1f
harshmangalam <harshdev8218@gmail.com>
2022-10-12 08:47:20 +0530

    added delete payment session

---
+++ carts/discount.go
@@ -0,0 +1,23 @@
+package carts
+
+import (
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+func DeleteDiscount(cartId string, code string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/carts/%v/discounts/%v", cartId, code)
+	resp, err := request.NewRequest().SetMethod(http.MethodDelete).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
--- carts/payment.go
+++ carts/payment.go
@@ -21,3 +21,16 @@ func CreatePaymentSession(cartId string, config *medusa.Config) ([]byte, error) 
 	}
 	return body, nil
 }
+
+func DeletePaymentSession(cartId, providerId string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/carts/%v/payment-sessions/%v", cartId, providerId)
+	resp, err := request.NewRequest().SetMethod(http.MethodDelete).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
