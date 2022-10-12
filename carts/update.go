ac09fc9e2bae18032e18a5486878bcba11851b0a
harshmangalam <harshdev8218@gmail.com>
2022-10-12 10:58:02 +0530

    added address

--- carts/update.go
+++ carts/update.go
@@ -1,11 +1,33 @@
 package carts
 
+import "time"
+
 type Discount struct {
 	Code string `json:"code"`
 }
 
 type GiftCard struct {
 	Code string `json:"code"`
+}
+
+type Address struct {
+	Id          string         `json:"id"`
+	CustomerId  string         `json:"customer_id"`
+	Customer    []any          `json:"customer"`
+	FirstName   []any          `json:"first_name"`
+	LastName    []any          `json:"last_name"`
+	Address1    []any          `json:"address_1"`
+	Address2    []any          `json:"address_2"`
+	City        []any          `json:"city"`
+	CountryCode []any          `json:"country_code"`
+	Country     any            `json:"country"`
+	Province    string         `json:"province"`
+	PostalCode  string         `json:"postal_code"`
+	Phone       string         `json:"phone"`
+	CreatedAt   time.Time      `json:"created_at,omitempty"`
+	UpdatedAt   time.Time      `json:"updated_at,omitempty"`
+	DeletedAt   time.Time      `json:"deleted_at,omitempty"`
+	Metadata    map[string]any `json:"metadata,omitempty"`
 }
 type UpdateCart struct {
 	RegionId       string         `json:"region_id"`
