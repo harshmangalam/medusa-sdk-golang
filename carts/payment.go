dbcb428871ecf72c0e17f939399653566fafe2a0
harshmangalam <harshdev8218@gmail.com>
2022-10-12 11:14:43 +0530

    update payment session

--- carts/payment.go
+++ carts/payment.go
@@ -13,6 +13,10 @@ type PaymentSession struct {
 	ProviderId string `json:"provider_id"`
 }
 
+type UpdatePaymentSession struct {
+	Data map[string]any `json:"data"`
+}
+
 func CreatePaymentSession(cartId string, config *medusa.Config) ([]byte, error) {
 	path := fmt.Sprintf("/store/carts/%v/payment-sessions", cartId)
 	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).Send(config)
@@ -63,6 +67,28 @@ func (p *PaymentSession) SetProviderId(providerId string) *PaymentSession {
 func (p *PaymentSession) Send(cartId string, config *medusa.Config) ([]byte, error) {
 	path := fmt.Sprintf("/store/carts/%v/payment-session", cartId)
 	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(p).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func NewUpdatePaymentSession() *UpdatePaymentSession {
+	return new(UpdatePaymentSession)
+}
+
+func (u *UpdatePaymentSession) SetData(data map[string]any) *UpdatePaymentSession {
+	u.Data = data
+	return u
+}
+
+func (u *UpdatePaymentSession) Apply(cartId, providerId string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/carts/%v/payment-sessions/%v", cartId, providerId)
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(u).Send(config)
 	if err != nil {
 		return nil, err
 	}
