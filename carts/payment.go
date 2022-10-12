773991164b7b3098b9d3dfea2010bf3fab7a0b0b
harshmangalam <harshdev8218@gmail.com>
2022-10-12 10:06:06 +0530

    set payment session

--- carts/payment.go
+++ carts/payment.go
@@ -9,6 +9,10 @@ import (
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
+type PaymentSession struct {
+	ProviderId string `json:"provider_id"`
+}
+
 func CreatePaymentSession(cartId string, config *medusa.Config) ([]byte, error) {
 	path := fmt.Sprintf("/store/carts/%v/payment-sessions", cartId)
 	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).Send(config)
@@ -25,6 +29,40 @@ func CreatePaymentSession(cartId string, config *medusa.Config) ([]byte, error) 
 func DeletePaymentSession(cartId, providerId string, config *medusa.Config) ([]byte, error) {
 	path := fmt.Sprintf("/store/carts/%v/payment-sessions/%v", cartId, providerId)
 	resp, err := request.NewRequest().SetMethod(http.MethodDelete).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func RefreshPaymentSession(cartId, providerId string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/carts/%v/payment-sessions/%v/refresh", cartId, providerId)
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func NewPaymentSession() *PaymentSession {
+	return new(PaymentSession)
+}
+
+func (p *PaymentSession) SetProviderId(providerId string) *PaymentSession {
+	p.ProviderId = providerId
+	return p
+}
+func (p *PaymentSession) Send(cartId string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/carts/%v/payment-session", cartId)
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(p).Send(config)
 	if err != nil {
 		return nil, err
 	}
