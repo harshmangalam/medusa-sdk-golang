e85c62adf8f49121d427fb3ade80845f5850beb4
harshmangalam <harshdev8218@gmail.com>
2022-10-13 10:23:03 +0530

    update customer billing addr

--- customers/update.go
+++ customers/update.go
@@ -1,17 +1,13 @@
 package customers
 
-import (
-	"github.com/harshmngalam/medusa-sdk-golang/address"
-)
-
 type UpdateCustomer struct {
-	Email          string             `json:"email"`
-	FirstName      string             `json:"first_name"`
-	LastName       string             `json:"last_name"`
-	BillingAddress []*address.Address `json:"billing_address"`
-	Phone          string             `json:"phone"`
-	Password       string             `json:"password"`
-	Metadata       any                `json:"metadata,omitempty"`
+	Email          string `json:"email"`
+	FirstName      string `json:"first_name"`
+	LastName       string `json:"last_name"`
+	BillingAddress any    `json:"billing_address"`
+	Phone          string `json:"phone"`
+	Password       string `json:"password"`
+	Metadata       any    `json:"metadata,omitempty"`
 }
 
 func NewUpdateCustomer() *UpdateCustomer {
@@ -32,7 +28,7 @@ func (u *UpdateCustomer) SetLastName(lastName string) *UpdateCustomer {
 	return u
 }
 
-func (u *UpdateCustomer) Set(_ string) *UpdateCustomer {
-
+func (u *UpdateCustomer) SetBillingAddress(billingAddr any) *UpdateCustomer {
+	u.BillingAddress = billingAddr
 	return u
 }
