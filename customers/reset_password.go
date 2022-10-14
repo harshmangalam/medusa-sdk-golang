1aa77a54f22a7bd742a1f17711c72943f69d79a7
harshmangalam <harshdev8218@gmail.com>
2022-10-14 11:36:07 +0530

    reset password

--- customers/reset_password.go
+++ customers/reset_password.go
@@ -1,5 +1,14 @@
 package customers
 
+import (
+	"encoding/json"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
 type ResetPassword struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
@@ -23,4 +32,24 @@ func (r *ResetPassword) SetPassword(password string) *ResetPassword {
 func (r *ResetPassword) SetToken(token string) *ResetPassword {
 	r.Token = token
 	return r
+}
+
+func (r *ResetPassword) Reset(config *medusa.Config) (*Customer, error) {
+	path := "/store/customers/password-reset"
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(r).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(ResponseBody)
+
+	if err := json.Unmarshal(body, respBody); err != nil {
+		return nil, err
+	}
+
+	return respBody.Customer, nil
 }
