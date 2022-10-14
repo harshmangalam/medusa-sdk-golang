35973bcc52316e2165c582eb589c64c19ec6167b
harshmangalam <harshdev8218@gmail.com>
2022-10-14 11:41:36 +0530

    added password reset token

--- customers/request_password_reset.go
+++ customers/request_password_reset.go
@@ -1,5 +1,13 @@
 package customers
 
+import (
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
 type RequestPasswordReset struct {
 	Email string `json:"email"`
 }
@@ -11,4 +19,18 @@ func NewRequestPasswordReset() *RequestPasswordReset {
 func (r *RequestPasswordReset) SetEmail(email string) *RequestPasswordReset {
 	r.Email = email
 	return r
+}
+
+func (r *ResetPassword) Send(config *medusa.Config) ([]byte, error) {
+	path := "/store/customers/password-token"
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(r).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
