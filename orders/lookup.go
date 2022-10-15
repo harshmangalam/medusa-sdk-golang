f8cf631731a4d70f00fb76c65aa76724f3adcc99
harshmangalam <harshdev8218@gmail.com>
2022-10-15 14:04:02 +0530

    lookup orders

--- orders/lookup.go
+++ orders/lookup.go
@@ -1,12 +1,23 @@
 package orders
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/google/go-querystring/query"
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
 type ShippingAddress struct {
 	PostalCode string `json:"postal_code,omitempty" url:"postal_code,omitempty"`
 }
 type Lookup struct {
 	DisplayId       string           `json:"display_id" url:"display_id"`
 	Email           string           `json:"email" url:"email"`
-	ShippingAddress *ShippingAddress `json:"shipping_address,omitempty"`
+	ShippingAddress *ShippingAddress `json:"shipping_address,omitempty" url:"shipping_address,omitempty"`
 }
 
 func NewLookup() *Lookup {
@@ -26,4 +37,32 @@ func (l *Lookup) SetEmail(email string) *Lookup {
 func (l *Lookup) SetShippingAddress(addr *ShippingAddress) *Lookup {
 	l.ShippingAddress = addr
 	return l
+}
+
+func (l *Lookup) Apply(cartId string, config *medusa.Config) (*Order, error) {
+	path := "/store/orders"
+	qs, err := query.Values(l)
+	if err != nil {
+		return nil, err
+	}
+
+	parseStr := qs.Encode()
+
+	path = fmt.Sprintf("%v?%v", path, parseStr)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(ResponseBody)
+
+	if err := json.Unmarshal(body, respBody); err != nil {
+		return nil, err
+	}
+
+	return respBody.Order, nil
 }
