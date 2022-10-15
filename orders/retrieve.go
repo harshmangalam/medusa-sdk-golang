93b4c3f5628669046d061a2246ef6dca7e35533e
harshmangalam <harshdev8218@gmail.com>
2022-10-15 13:47:23 +0530

    retrieve order by id

---
+++ orders/retrieve.go
@@ -0,0 +1,31 @@
+package orders
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+func Retrieve(id string, config *medusa.Config) (*Order, error) {
+	path := fmt.Sprintf("/store/orders/%v", id)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(ResponseBody)
+
+	if err := json.Unmarshal(body, respBody); err != nil {
+		return nil, err
+	}
+
+	return respBody.Order, nil
+}
