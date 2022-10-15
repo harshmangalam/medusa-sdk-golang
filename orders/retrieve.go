cdab06e9c622be04fca10ea1380a255b9683972d
harshmangalam <harshdev8218@gmail.com>
2022-10-15 13:52:31 +0530

    retrieve by cart id

--- orders/retrieve.go
+++ orders/retrieve.go
@@ -29,3 +29,23 @@ func Retrieve(id string, config *medusa.Config) (*Order, error) {
 
 	return respBody.Order, nil
 }
+
+func RetrieveByCartId(cartId string, config *medusa.Config) (*Order, error) {
+	path := fmt.Sprintf("/store/orders/cart/%v", cartId)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(ResponseBody)
+
+	if err := json.Unmarshal(body, respBody); err != nil {
+		return nil, err
+	}
+
+	return respBody.Order, nil
+}
