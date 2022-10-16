5b10946b44b192ff798778248870333c602fc8bd
harshmangalam <harshdev8218@gmail.com>
2022-10-16 08:17:26 +0530

    retrieve gift card

--- gift_cards/gift_card.go
+++ gift_cards/gift_card.go
@@ -1,9 +1,15 @@
 package giftcards
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"time"
 
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/orders"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
 type GiftCard struct {
@@ -25,4 +31,24 @@ type GiftCard struct {
 
 type ResponseBody struct {
 	GiftCard *GiftCard `json:"gift_card"`
+}
+
+func Retrieve(code string, config *medusa.Config) (*GiftCard, error) {
+	path := fmt.Sprintf("/store/gift-cards/%v", code)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(ResponseBody)
+
+	if err := json.Unmarshal(body, respBody); err != nil {
+		return nil, err
+	}
+
+	return respBody.GiftCard, nil
 }
