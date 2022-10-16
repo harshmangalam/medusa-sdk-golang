f73f3c7a43c133163b5ea491b9905bf6178d8eaf
harshmangalam <harshdev8218@gmail.com>
2022-10-16 08:38:06 +0530

    ipdated prosuct schema

--- products/products.go
+++ products/products.go
@@ -3,6 +3,7 @@ package products
 import (
 	"github.com/harshmngalam/medusa-sdk-golang/collections"
 	"github.com/harshmngalam/medusa-sdk-golang/common"
+	productvariants "github.com/harshmngalam/medusa-sdk-golang/product_variants"
 )
 
 type ProductStatus string
@@ -43,35 +44,6 @@ type ProductOption struct {
 	Metadata  map[string]any `json:"metadata,omitempty"`
 }
 
-type ProductVariant struct {
-	Title             string           `json:"title"`
-	ProductId         string           `json:"product_id"`
-	InventoryQuantity int              `json:"inventory_quantity"`
-	Id                string           `json:"id,omitempty"`
-	Product           *Product         `json:"product"`
-	Prices            []any            `json:"prices"`
-	Sku               string           `json:"sku,omitempty"`
-	Barcode           string           `json:"bardcode,omitempty"`
-	Ean               string           `json:"ean,omitempty"`
-	Upc               string           `json:"upc,omitempty"`
-	VariatRank        int              `json:"variant_rank,omitempty"`
-	AllowBackorder    bool             `json:"allow_backorder"`
-	ManageInventory   bool             `json:"manage_inventory"`
-	HsCode            string           `json:"hs_code"`
-	OriginCountry     string           `json:"origin_country"`
-	MidCode           string           `json:"mid_code"`
-	Material          string           `json:"material"`
-	Weight            int              `json:"weight"`
-	Height            int              `json:"height"`
-	Width             int              `json:"width"`
-	Length            int              `json:"length"`
-	Options           []*ProductOption `json:"options"`
-	CreatedAt         string           `json:"created_at"`
-	UpdatedAt         string           `json:"updated_at"`
-	DeletedAt         string           `json:"deleted_at"`
-	Metadata          map[string]any   `json:"metadata"`
-}
-
 type ProductType struct {
 	Value     string         `json:"value"`
 	Id        string         `json:"id"`
@@ -91,23 +63,23 @@ type Tag struct {
 }
 
 type Product struct {
-	Title         string            `json:"title"`
-	ProfileId     string            `json:"profile_id"`
-	Id            string            `json:"id"`
-	Subtitle      string            `json:"subtitle"`
-	Description   string            `json:"description"`
-	Handle        string            `json:"handle"`
-	IsGiftcard    bool              `json:"is_giftcard"`
-	Status        ProductStatus     `json:"status"`
-	Images        []*common.Image   `json:"images"`
-	Thumbnail     string            `json:"thumbnail"`
-	Options       []*ProductOption  `json:"options"`
-	Variants      []*ProductVariant `json:"variants"`
-	Profile       *ShippingProfile  `json:"profile"`
-	Weight        int               `json:"weight"`
-	Height        int               `json:"height"`
-	Width         int               `json:"width"`
-	Length        int               `json:"length"`
+	Title         string                            `json:"title"`
+	ProfileId     string                            `json:"profile_id"`
+	Id            string                            `json:"id"`
+	Subtitle      string                            `json:"subtitle"`
+	Description   string                            `json:"description"`
+	Handle        string                            `json:"handle"`
+	IsGiftcard    bool                              `json:"is_giftcard"`
+	Status        ProductStatus                     `json:"status"`
+	Images        []*common.Image                   `json:"images"`
+	Thumbnail     string                            `json:"thumbnail"`
+	Options       []*productvariants.ProductVariant `json:"options"`
+	Variants      []*productvariants.ProductVariant `json:"variants"`
+	Profile       *ShippingProfile                  `json:"profile"`
+	Weight        int                               `json:"weight"`
+	Height        int                               `json:"height"`
+	Width         int                               `json:"width"`
+	Length        int                               `json:"length"`
 	HsCode        string
 	OriginCountry string                  `json:"origin_country"`
 	MidCode       string                  `json:"mid_code"`
