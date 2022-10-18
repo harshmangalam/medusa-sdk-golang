2df491bd45e078e43d62f0b3b1ccb57950f668c5
harshmangalam <harshdev8218@gmail.com>
2022-10-18 08:24:06 +0530

    added list method

--- shipping_options/list.go
+++ shipping_options/list.go
@@ -1,5 +1,15 @@
 package shippingoptions
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/google/go-querystring/query"
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
 type ListCartOptionsQuery struct {
 	Isreturn   bool   `json:"is_return,omitempty" url:"is_return,omitempty"`
 	ProductIds string `json:"product_ids,omitempty" url:"product_ids,omitempty"`
@@ -23,4 +33,32 @@ func (l *ListCartOptionsQuery) SetProductIds(productIds string) *ListCartOptions
 func (l *ListCartOptionsQuery) SetRegionId(regionId string) *ListCartOptionsQuery {
 	l.RegionId = regionId
 	return l
+}
+
+func (l *ListCartOptionsQuery) List(config *medusa.Config) ([]byte, error) {
+	path := "/store/shipping-options"
+
+	qs, err := query.Values(l)
+	if err != nil {
+		return nil, err
+	}
+
+	parseStr := qs.Encode()
+
+	path = fmt.Sprintf("%v?%v", path, parseStr)
+
+	resp, err := request.
+		NewRequest().
+		SetMethod(http.MethodGet).
+		SetPath(path).
+		Send(config)
+
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
