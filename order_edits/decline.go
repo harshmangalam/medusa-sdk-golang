900bd3a4aa5f26b34bdb199051ac00f13aef064a
harshmangalam <harshdev8218@gmail.com>
2022-10-18 14:42:08 +0530

    add decline reason

--- order_edits/decline.go
+++ order_edits/decline.go
@@ -1,5 +1,14 @@
 package orderedits
 
-type Decline struct {
+type DeclineBody struct {
 	DeclinedReason string `json:"declined_reason,omitempty"`
+}
+
+func NewDeclineBody() *DeclineBody {
+	return new(DeclineBody)
+}
+
+func (d *DeclineBody) SetDeclineReason(reason string) *DeclineBody {
+	d.DeclinedReason = reason
+	return d
 }
