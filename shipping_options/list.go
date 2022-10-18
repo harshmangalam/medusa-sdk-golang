c9bb7f432e31ffa626ae78eeac36efbc0845c7db
harshmangalam <harshdev8218@gmail.com>
2022-10-18 08:21:09 +0530

    added methods for list cart options

--- shipping_options/list.go
+++ shipping_options/list.go
@@ -1,11 +1,26 @@
 package shippingoptions
 
 type ListCartOptionsQuery struct {
-	Isreturn   bool `json:"is_return,omitempty" url:"is_return,omitempty"`
-	ProductIds bool `json:"product_ids,omitempty" url:"product_ids,omitempty"`
-	RegionId   bool `json:"region_id,omitempty" url:"region_id,omitempty"`
+	Isreturn   bool   `json:"is_return,omitempty" url:"is_return,omitempty"`
+	ProductIds string `json:"product_ids,omitempty" url:"product_ids,omitempty"`
+	RegionId   string `json:"region_id,omitempty" url:"region_id,omitempty"`
 }
 
 func NewListCartOptionsQuery() *ListCartOptionsQuery {
 	return new(ListCartOptionsQuery)
+}
+
+func (l *ListCartOptionsQuery) SetIsreturn(isReturn bool) *ListCartOptionsQuery {
+	l.Isreturn = isReturn
+	return l
+}
+
+func (l *ListCartOptionsQuery) SetProductIds(productIds string) *ListCartOptionsQuery {
+	l.ProductIds = productIds
+	return l
+}
+
+func (l *ListCartOptionsQuery) SetRegionId(regionId string) *ListCartOptionsQuery {
+	l.RegionId = regionId
+	return l
 }
