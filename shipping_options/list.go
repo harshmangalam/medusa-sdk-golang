a5852784f5327934c04176c6f685da5a9667975e
harshmangalam <harshdev8218@gmail.com>
2022-10-18 07:56:09 +0530

    added shipping options list

---
+++ shipping_options/list.go
@@ -0,0 +1,28 @@
+package shippingoptions
+
+import (
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+func List(cartId string, config *medusa.Config) ([]byte, error) {
+	path := "/store/shipping-options"
+
+	resp, err := request.
+		NewRequest().
+		SetMethod(http.MethodGet).
+		SetPath(path).
+		Send(config)
+
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
