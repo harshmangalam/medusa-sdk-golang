25de76940cd75b2fbf4dd9866265383c2aaf3a57
harshmangalam <harshdev8218@gmail.com>
2022-10-18 14:44:44 +0530

    added decline method

--- order_edits/decline.go
+++ order_edits/decline.go
@@ -1,5 +1,15 @@
 package orderedits
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
 type DeclineBody struct {
 	DeclinedReason string `json:"declined_reason,omitempty"`
 }
@@ -11,4 +21,24 @@ func NewDeclineBody() *DeclineBody {
 func (d *DeclineBody) SetDeclineReason(reason string) *DeclineBody {
 	d.DeclinedReason = reason
 	return d
+}
+
+func (d *DeclineBody) Decline(id string, config *medusa.Config) (*OrderEdit, error) {
+	path := fmt.Sprintf("/store/order-edits/%v/decline", id)
+	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(d).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(RetrieveOrderEditResponse)
+
+	if err := json.Unmarshal(body, respBody); err != nil {
+		return nil, err
+	}
+
+	return respBody.OrderEdit, nil
 }
