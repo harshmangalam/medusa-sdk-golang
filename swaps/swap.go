94bb91197d59e5fe4d0c15fa6674f716afaa99dd
harshmangalam <harshdev8218@gmail.com>
2022-10-19 12:20:31 +0530

    added fulfilment status

--- swaps/swap.go
+++ swaps/swap.go
@@ -7,31 +7,41 @@ import (
 	"github.com/harshmngalam/medusa-sdk-golang/orders"
 )
 
+type FulfillmentStatus string
+
+const (
+	NotFulfilled   FulfillmentStatus = "not_fulfilled"
+	Fulfilled      FulfillmentStatus = "fulfilled"
+	Shipped        FulfillmentStatus = "shipped"
+	Canceled       FulfillmentStatus = "canceled"
+	RequiresAction FulfillmentStatus = "requires_action"
+)
+
 type Swap struct {
-	FulfillmentStatus string           `json:"fulfillment_status"`
-	PaymentStatus     string           `json:"payment_status"`
-	OrderId           string           `json:"order_id"`
-	Id                string           `json:"id"`
-	Order             *orders.Order    `json:"order"`
-	AdditionalItems   []any            `json:"additional_items"`
-	ReturnOrder       any              `json:"eturn_order"`
-	Fulfillments      any              `json:"fulfillments"`
-	Payment           any              `json:"payment"`
-	DifferenceDue     int              `json:"difference_due"`
-	ShippingaddressId string           `json:"shipping_address_id"`
-	ShippingAddress   *address.Address `json:"shipping_address"`
-	ShippingMethods   []any            `json:"shipping_methods"`
-	CartId            string           `json:"cart_id"`
-	Cart              any              `json:"cart"`
-	AllowBackorder    bool             `json:"allow_backorder"`
-	IdempotencyKey    string           `json:"idempotency_key"`
-	ConfirmedAt       *time.Time       `json:"confirmed_at"`
-	CanceledAt        *time.Time       `json:"canceled_at"`
-	NoNotification    bool             `json:"no_notification"`
-	CreatedAt         *time.Time       `json:"created_at"`
-	UpdatedAt         *time.Time       `json:"updated_at"`
-	DeletedAt         *time.Time       `json:"deleted_at"`
-	Metadata          map[string]any   `json:"metadata"`
+	FulfillmentStatus FulfillmentStatus `json:"fulfillment_status"`
+	PaymentStatus     string            `json:"payment_status"`
+	OrderId           string            `json:"order_id"`
+	Id                string            `json:"id"`
+	Order             *orders.Order     `json:"order"`
+	AdditionalItems   []any             `json:"additional_items"`
+	ReturnOrder       any               `json:"eturn_order"`
+	Fulfillments      any               `json:"fulfillments"`
+	Payment           any               `json:"payment"`
+	DifferenceDue     int               `json:"difference_due"`
+	ShippingaddressId string            `json:"shipping_address_id"`
+	ShippingAddress   *address.Address  `json:"shipping_address"`
+	ShippingMethods   []any             `json:"shipping_methods"`
+	CartId            string            `json:"cart_id"`
+	Cart              any               `json:"cart"`
+	AllowBackorder    bool              `json:"allow_backorder"`
+	IdempotencyKey    string            `json:"idempotency_key"`
+	ConfirmedAt       *time.Time        `json:"confirmed_at"`
+	CanceledAt        *time.Time        `json:"canceled_at"`
+	NoNotification    bool              `json:"no_notification"`
+	CreatedAt         *time.Time        `json:"created_at"`
+	UpdatedAt         *time.Time        `json:"updated_at"`
+	DeletedAt         *time.Time        `json:"deleted_at"`
+	Metadata          map[string]any    `json:"metadata"`
 }
 
 type ResponseBody struct {
