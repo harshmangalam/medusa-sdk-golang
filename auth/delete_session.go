f98a471d771f6b22133d3d4cbbd6413b0fbfa878
harshmangalam <harshdev8218@gmail.com>
2022-10-20 13:35:11 +0530

    added delete session

--- auth/delete_session.go
+++ auth/delete_session.go
@@ -1,15 +1,28 @@
 package auth
 
 import (
+	"encoding/json"
 	"net/http"
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
-// logout current active user
-func DeleteSession(config *medusa.Config) ([]byte, error) {
+type DeleteSessionResponse struct {
+	// Success response
+	Data any
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+// Logout current active user
+func DeleteSession(config *medusa.Config) (*DeleteSessionResponse, error) {
 
 	path := "/store/auth"
 	resp, err := request.NewRequest().SetMethod(http.MethodDelete).SetPath(path).Send(config)
@@ -17,9 +30,41 @@ func DeleteSession(config *medusa.Config) ([]byte, error) {
 		return nil, err
 	}
 	body, err := utils.ParseResponseBody(resp)
-	if err != nil {
-		return nil, err
+	respBody := new(DeleteSessionResponse)
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respBody.Data = nil
+
+	case http.StatusUnauthorized:
+		respErr := new(response.Error)
+		respErr.Message = "Unauthorized"
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors := new(response.Errors)
+		if json.Unmarshal(body, respErrors); err != nil {
+			return nil, err
+		}
+
+		if len(respErrors.Errors) == 0 {
+			respError := new(response.Error)
+			if json.Unmarshal(body, respError); err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr := new(response.Error)
+		if json.Unmarshal(body, respErr); err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+
 	}
 
-	return body, nil
+	return respBody, nil
 }
