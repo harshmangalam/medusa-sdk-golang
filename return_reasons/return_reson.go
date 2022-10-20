4503621bb24f194984660ed32094e5c898e6d974
harshmangalam <harshdev8218@gmail.com>
2022-10-20 09:46:26 +0530

    added return reason schema

---
+++ return_reasons/return_reson.go
@@ -0,0 +1,17 @@
+package returnreasons
+
+import "time"
+
+type ReturnReason struct {
+	Value                string         `json:"value"`
+	Label                string         `json:"Label"`
+	Id                   string         `json:"id"`
+	Description          string         `json:"description"`
+	ParentReturnReason   *ReturnReason  `json:"parent_return_reason"`
+	ParentReturnReasonId string         `json:"parent_return_reason_id"`
+	ReturnReasonChildren *ReturnReason  `json:"return_reason_children"`
+	CreatedAt            *time.Time     `json:"created_at"`
+	UpdatedAt            *time.Time     `json:"updated_at"`
+	DeletedAt            *time.Time     `json:"deleted_at"`
+	Metadata             map[string]any `json:"metadata"`
+}
