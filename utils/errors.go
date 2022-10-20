c2115433e1711d74f9c16eeb19fd0d3aac1fcd36
harshmangalam <harshdev8218@gmail.com>
2022-10-20 13:56:22 +0530

    code formatted

--- utils/errors.go
+++ utils/errors.go
@@ -11,22 +11,17 @@ func ParseErrors(body []byte) (*response.Errors, error) {
 	if err := json.Unmarshal(body, respErrors); err != nil {
 		return nil, err
 	}
-
 	return respErrors, nil
 }
-
 func ParseError(body []byte) (*response.Error, error) {
 	respError := new(response.Error)
 	if err := json.Unmarshal(body, respError); err != nil {
 		return nil, err
 	}
-
 	return respError, nil
 }
-
 func UnauthorizeError() *response.Error {
 	respErr := new(response.Error)
 	respErr.Message = "Unauthorized"
-
 	return respErr
 }
