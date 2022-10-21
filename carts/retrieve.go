8bc449ce8f36ce82f82df2774031eb77eff12ec9
harshmangalam <harshdev8218@gmail.com>
2022-10-21 10:19:46 +0530

    retrieve cart

---
+++ carts/retrieve.go
@@ -0,0 +1,23 @@
+package carts
+
+import (
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+func Retrieve(cartId string, config *medusa.Config) ([]byte, error) {
+	path := fmt.Sprintf("/store/carts/%v", cartId)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
