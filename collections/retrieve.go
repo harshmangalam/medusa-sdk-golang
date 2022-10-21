685b5947de576ecca15d7b0dc72dcf8ad9a02904
harshmangalam <harshdev8218@gmail.com>
2022-10-21 19:52:49 +0530

    added retrieve collection response

--- collections/retrieve.go
+++ collections/retrieve.go
@@ -6,8 +6,26 @@ import (
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
+
+type RetrieveCollectionData struct {
+	// Array of collection
+	Collection *schema.Collection `json:"collection"`
+}
+
+type RetrieveCollectionResponse struct {
+	// Success response
+	Data *RetrieveCollectionData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
 
 // Retrieves a Product Collection.
 
