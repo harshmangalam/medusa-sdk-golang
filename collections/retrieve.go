c9e8924309f7119346514f8fcf3002eda9e31e46
harshmangalam <harshdev8218@gmail.com>
2022-10-21 19:53:47 +0530

    added retrieve collection errors

--- collections/retrieve.go
+++ collections/retrieve.go
@@ -1,6 +1,7 @@
 package collections
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -29,7 +30,7 @@ type RetrieveCollectionResponse struct {
 
 // Retrieves a Product Collection.
 
-func Retrieve(id string, config *medusa.Config) ([]byte, error) {
+func Retrieve(id string, config *medusa.Config) (*RetrieveCollectionResponse, error) {
 	path := fmt.Sprintf("/store/collections/%v", id)
 	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
 	if err != nil {
@@ -39,6 +40,42 @@ func Retrieve(id string, config *medusa.Config) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return body, nil
+	respBody := new(RetrieveCollectionResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(RetrieveCollectionData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
 
 }
