8b123cc15ac4c7fb39181fb383999bbe97ac7b06
harshmangalam <harshdev8218@gmail.com>
2022-10-21 18:42:21 +0530

    added select payment response

--- carts/select_payment_session.go
+++ carts/select_payment_session.go
@@ -1,13 +1,30 @@
 package carts
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
+
+type SelectPaymentSessionData struct {
+	Cart *Cart `json:"cart"`
+}
+
+type SelectPaymentSessionResponse struct {
+	// Success response
+	Data *SelectPaymentSessionData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
 
 type SelectPaymentSession struct {
 	ProviderId string `json:"provider_id"`
@@ -21,7 +38,7 @@ func (p *SelectPaymentSession) SetProviderId(providerId string) *SelectPaymentSe
 	p.ProviderId = providerId
 	return p
 }
-func (p *SelectPaymentSession) Select(cartId string, config *medusa.Config) ([]byte, error) {
+func (p *SelectPaymentSession) Select(cartId string, config *medusa.Config) (*SelectPaymentSessionResponse, error) {
 	path := fmt.Sprintf("/store/carts/%v/payment-session", cartId)
 	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(p).Send(config)
 	if err != nil {
@@ -31,5 +48,41 @@ func (p *SelectPaymentSession) Select(cartId string, config *medusa.Config) ([]b
 	if err != nil {
 		return nil, err
 	}
-	return body, nil
+	respBody := new(SelectPaymentSessionResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(SelectPaymentSessionData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
 }
