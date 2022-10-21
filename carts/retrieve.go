ae5857e5eaf41384141c979f042f96d82fe95a1c
harshmangalam <harshdev8218@gmail.com>
2022-10-21 18:57:01 +0530

    added retrieve cart response

--- carts/retrieve.go
+++ carts/retrieve.go
@@ -1,16 +1,33 @@
 package carts
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
+type RetrieveData struct {
+	Cart *Cart `json:"cart"`
+}
+
+type RetrieveResponse struct {
+	// Success response
+	Data *RetrieveData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
 // Retrieves a Cart.
-func Retrieve(cartId string, config *medusa.Config) ([]byte, error) {
+func Retrieve(cartId string, config *medusa.Config) (*RetrieveResponse, error) {
 	path := fmt.Sprintf("/store/carts/%v", cartId)
 	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
 	if err != nil {
@@ -20,5 +37,41 @@ func Retrieve(cartId string, config *medusa.Config) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return body, nil
+	respBody := new(RetrieveResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(RetrieveData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
 }
