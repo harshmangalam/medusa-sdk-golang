b9d673e7bc358495ab074fb54c33786a17994b4f
harshmangalam <harshdev8218@gmail.com>
2022-10-21 15:51:50 +0530

    added cart doc comment

--- carts/cart.go
+++ carts/cart.go
@@ -9,42 +9,80 @@ import (
 )
 
 type Cart struct {
-	Id                  string                `json:"id"`
-	Email               string                `json:"email"`
-	BillingddressId     string                `json:"billing_address_id"`
-	BillingAddress      *address.Address      `json:"billing_address"`
-	ShippingAddressId   string                `json:"shipping_address_id"`
-	ShippingAddress     *address.Address      `json:"shipping_address"`
-	Items               []any                 `json:"items"`
-	RegionId            string                `json:"region_id"`
-	Region              any                   `json:"region"`
-	Discounts           []any                 `json:"discounts"`
-	GiftCards           []*giftcards.GiftCard `json:"gift_cards"`
-	CustomerId          string                `json:"customer_id"`
-	Customer            *customers.Customer   `json:"customer"`
-	PaymentSession      any                   `json:"payment_session"`
-	PaymentSessions     []any                 `json:"payment_sessions"`
-	PaymentId           string                `json:"payment_id"`
-	Payment             any                   `json:"payment"`
-	ShippingMethods     any                   `json:"shipping_methods"`
-	Type                any                   `json:"type"`
-	CompletedAt         *time.Time            `json:"completed_at"`
-	PaymentAuthorizedAt *time.Time            `json:"payment_authorized_at"`
-	Idempotency_key     string                `json:"idempotency_key"`
-	Context             map[string]any        `json:"context"`
-	SalesChannelId      string                `json:"sales_channel_id"`
-	SalesChannel        any                   `json:"sales_channel"`
-	CreatedAt           *time.Time            `json:"created_at,omitempty"`
-	UpdatedAt           *time.Time            `json:"updated_at,omitempty"`
-	DeletedAt           *time.Time            `json:"deleted_at,omitempty"`
-	Metadata            any                   `json:"metadata,omitempty"`
-	ShippingTotal       uint                  `json:"shipping_total"`
-	DiscountTotal       uint                  `json:"discount_total"`
-	TaxTotal            uint                  `json:"tax_total"`
-	RefundedTotal       uint                  `json:"refunded_total"`
-	Total               uint                  `json:"total"`
-	Subtotal            uint                  `json:"subtotal"`
-	RefundableAmount    uint                  `json:"refundable_amount"`
-	GiftCardTaxTotal    uint                  `json:"gift_card_tax_total"`
-	GiftCardTotal       uint                  `json:"gift_card_total"`
+	// The cart's ID
+	Id string `json:"id"`
+
+	// The email associated with the cart
+	Email string `json:"email"`
+
+	// The billing address's ID
+	BillingddressId string `json:"billing_address_id"`
+
+	// An address.
+	BillingAddress *address.Address `json:"billing_address"`
+
+	// The shipping address's ID
+	ShippingAddressId string `json:"shipping_address_id"`
+
+	// An address.
+	ShippingAddress *address.Address `json:"shipping_address"`
+
+	// Available if the relation items is expanded.
+	Items []any `json:"items"`
+
+	// The region's ID
+	RegionId string `json:"region_id"`
+
+	// A region object. Available if the relation region is expanded.
+	Region any `json:"region"`
+
+	// Available if the relation discounts is expanded.
+	Discounts []any `json:"discounts"`
+
+	// Available if the relation gift_cards is expanded
+	GiftCards []*giftcards.GiftCard `json:"gift_cards"`
+
+	// The customer's ID
+	CustomerId string `json:"customer_id"`
+
+	// A customer object. Available if the relation customer is expanded.
+	Customer *customers.Customer `json:"customer"`
+
+	// Payment Sessions are created when a Customer initilizes the checkout flow, and can be used to hold the state of a payment flow. Each Payment Session is controlled by a Payment Provider, who is responsible for the communication with external payment services. Authorized Payment Sessions will eventually get promoted to Payments to indicate that they are authorized for capture/refunds/etc.
+
+	PaymentSession any `json:"payment_session"`
+
+	// The payment sessions created on the cart.
+	PaymentSessions []any `json:"payment_sessions"`
+
+	// The payment's ID if available
+	PaymentId string `json:"payment_id"`
+
+	// Payments represent an amount authorized with a given payment method, Payments can be captured, canceled or refunded.
+	Payment any `json:"payment"`
+
+	// The shipping methods added to the cart.
+	ShippingMethods any `json:"shipping_methods"`
+
+	// The cart's type.
+	Type                any            `json:"type"`
+	CompletedAt         *time.Time     `json:"completed_at"`
+	PaymentAuthorizedAt *time.Time     `json:"payment_authorized_at"`
+	Idempotency_key     string         `json:"idempotency_key"`
+	Context             map[string]any `json:"context"`
+	SalesChannelId      string         `json:"sales_channel_id"`
+	SalesChannel        any            `json:"sales_channel"`
+	CreatedAt           *time.Time     `json:"created_at,omitempty"`
+	UpdatedAt           *time.Time     `json:"updated_at,omitempty"`
+	DeletedAt           *time.Time     `json:"deleted_at,omitempty"`
+	Metadata            any            `json:"metadata,omitempty"`
+	ShippingTotal       uint           `json:"shipping_total"`
+	DiscountTotal       uint           `json:"discount_total"`
+	TaxTotal            uint           `json:"tax_total"`
+	RefundedTotal       uint           `json:"refunded_total"`
+	Total               uint           `json:"total"`
+	Subtotal            uint           `json:"subtotal"`
+	RefundableAmount    uint           `json:"refundable_amount"`
+	GiftCardTaxTotal    uint           `json:"gift_card_tax_total"`
+	GiftCardTotal       uint           `json:"gift_card_total"`
 }
