9f5d1ab7918b0f23549b23e7d7ef82430b20fce8
harshmangalam <harshdev8218@gmail.com>
2022-10-21 15:33:51 +0530

    added complete cart response

--- carts/complete.go
+++ carts/complete.go
@@ -6,8 +6,25 @@ import (
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
+
+type CompleteData struct {
+	Type any `json:"type"`
+	Data any `json:"data"`
+}
+
+type CompleteResponse struct {
+	// Success response
+	Data *CompleteData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
 
 func Complete(cart_id string, config *medusa.Config) ([]byte, error) {
 	path := fmt.Sprintf("/store/carts/%v/complete", cart_id)
