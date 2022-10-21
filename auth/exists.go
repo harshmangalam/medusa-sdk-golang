310ed6756f4a3e8e509ed181ff635ae9cdea93b6
harshmangalam <harshdev8218@gmail.com>
2022-10-21 10:11:01 +0530

    added exists response data

---
+++ auth/exists.go
@@ -0,0 +1,79 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+type ExistsData struct {
+	// Whether email exists or not.
+	Exists bool `json:"exists"`
+}
+
+type ExistsResponse struct {
+	// Success response
+	Data *ExistsData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+// verify customer emaail address
+func Exists(email string, config *medusa.Config) (*ExistsResponse, error) {
+	path := fmt.Sprintf("/store/auth/%v", email)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(ExistsResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(ExistsData)
+		if json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
+}
