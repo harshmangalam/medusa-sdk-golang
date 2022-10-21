01ccd47c6333244ae0c04587f9e6fefc136836ad
harshmangalam <harshdev8218@gmail.com>
2022-10-21 18:33:21 +0530

    update payment session data response

--- carts/payment.go
+++ carts/payment.go
@@ -13,10 +13,6 @@ type PaymentSession struct {
 	ProviderId string `json:"provider_id"`
 }
 
-type UpdatePaymentSession struct {
-	Data map[string]any `json:"data"`
-}
-
 func NewPaymentSession() *PaymentSession {
 	return new(PaymentSession)
 }
@@ -28,28 +24,6 @@ func (p *PaymentSession) SetProviderId(providerId string) *PaymentSession {
 func (p *PaymentSession) Send(cartId string, config *medusa.Config) ([]byte, error) {
 	path := fmt.Sprintf("/store/carts/%v/payment-session", cartId)
 	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(p).Send(config)
-	if err != nil {
-		return nil, err
-	}
-	body, err := utils.ParseResponseBody(resp)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
-}
-
-func NewUpdatePaymentSession() *UpdatePaymentSession {
-	return new(UpdatePaymentSession)
-}
-
-func (u *UpdatePaymentSession) SetData(data map[string]any) *UpdatePaymentSession {
-	u.Data = data
-	return u
-}
-
-func (u *UpdatePaymentSession) Apply(cartId, providerId string, config *medusa.Config) ([]byte, error) {
-	path := fmt.Sprintf("/store/carts/%v/payment-sessions/%v", cartId, providerId)
-	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(u).Send(config)
 	if err != nil {
 		return nil, err
 	}
--- carts/update_payment_session.go
+++ carts/update_payment_session.go
@@ -1,13 +1,30 @@
 package carts
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
+
+type UpdatePaymentSessionData struct {
+	Cart *Cart `json:"cart"`
+}
+
+type UpdatePaymentSessionResponse struct {
+	// Success response
+	Data *UpdatePaymentSessionData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
 
 type UpdatePaymentSession struct {
 	Data map[string]any `json:"data"`
@@ -22,7 +39,7 @@ func (u *UpdatePaymentSession) SetData(data map[string]any) *UpdatePaymentSessio
 	return u
 }
 
-func (u *UpdatePaymentSession) Update(cartId, providerId string, config *medusa.Config) ([]byte, error) {
+func (u *UpdatePaymentSession) Update(cartId, providerId string, config *medusa.Config) (*UpdatePaymentSessionResponse, error) {
 	path := fmt.Sprintf("/store/carts/%v/payment-sessions/%v", cartId, providerId)
 	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(u).Send(config)
 	if err != nil {
@@ -32,5 +49,41 @@ func (u *UpdatePaymentSession) Update(cartId, providerId string, config *medusa.
 	if err != nil {
 		return nil, err
 	}
-	return body, nil
+	respBody := new(UpdatePaymentSessionResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(UpdatePaymentSessionData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
 }
