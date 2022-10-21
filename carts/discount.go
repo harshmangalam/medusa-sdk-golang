c3c92260284b650f73a2102fd5fa8fde00933ed2
harshmangalam <harshdev8218@gmail.com>
2022-10-21 17:59:35 +0530

    added delete discount code response

--- carts/discount.go
+++ carts/discount.go
@@ -1,15 +1,32 @@
 package carts
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
-func DeleteDiscount(cartId string, code string, config *medusa.Config) ([]byte, error) {
+type DeleteDiscountData struct {
+	Cart *Cart `json:"cart"`
+}
+
+type DeleteDiscountResponse struct {
+	// Success response
+	Data *DeleteDiscountData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+func DeleteDiscount(cartId string, code string, config *medusa.Config) (*DeleteDiscountResponse, error) {
 	path := fmt.Sprintf("/store/carts/%v/discounts/%v", cartId, code)
 	resp, err := request.NewRequest().SetMethod(http.MethodDelete).SetPath(path).Send(config)
 	if err != nil {
@@ -19,5 +36,41 @@ func DeleteDiscount(cartId string, code string, config *medusa.Config) ([]byte, 
 	if err != nil {
 		return nil, err
 	}
-	return body, nil
+	respBody := new(DeleteDiscountResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(DeleteDiscountData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
 }
