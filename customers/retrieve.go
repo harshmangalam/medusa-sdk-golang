49ad38300d61084e820b603ca455fe8f91ad764e
harshmangalam <harshdev8218@gmail.com>
2022-10-22 18:21:00 +0530

    added retrieve customer response schema

--- customers/retrieve.go
+++ customers/retrieve.go
@@ -1,15 +1,31 @@
 package customers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
-func Retrieve(config *medusa.Config) (*Customer, error) {
+type RetrieveCustomerData struct {
+	Customer *schema.Customer `json:"customer"`
+}
+
+type RetrieveCustomerResponse struct {
+	// Success response
+	Data *RetrieveCustomerData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+func Retrieve(config *medusa.Config) (*RetrieveCustomerResponse, error) {
 	path := "/store/customers/me"
 	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
 	if err != nil {
@@ -20,11 +36,4 @@ func Retrieve(config *medusa.Config) (*Customer, error) {
 		return nil, err
 	}
 
-	respBody := new(ResponseBody)
-
-	if err := json.Unmarshal(body, respBody); err != nil {
-		return nil, err
-	}
-
-	return respBody.Customer, nil
 }
