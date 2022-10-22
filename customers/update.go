0a2407946ab7dba2ce991e4d5e62f99e46994541
harshmangalam <harshdev8218@gmail.com>
2022-10-22 18:24:01 +0530

    added update customer response schema

--- customers/update.go
+++ customers/update.go
@@ -6,8 +6,25 @@ import (
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
+
+type UpdateCustomerData struct {
+	Customer *schema.Customer `json:"customer"`
+}
+
+type UpdateCustomerResponse struct {
+	// Success response
+	Data *UpdateCustomerData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
 
 type UpdateCustomer struct {
 	Email          string         `json:"email,omitempty"`
