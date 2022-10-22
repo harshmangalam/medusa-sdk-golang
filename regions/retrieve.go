a459b74a93032e5950b0c7c864fbbde14a335a98
harshmangalam <harshdev8218@gmail.com>
2022-10-22 08:45:18 +0530

    added retrieve region rsponse schema

--- regions/retrieve.go
+++ regions/retrieve.go
@@ -1,16 +1,32 @@
 package regions
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
-func Retrieve(regionId string, config *medusa.Config) (*Region, error) {
+type RetrieveRegiondata struct {
+	Region []*schema.Region `json:"region"`
+}
+
+type RetrieveRegionResponse struct {
+	// Success response
+	Data *RetrieveRegiondata
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+func Retrieve(regionId string, config *medusa.Config) (*RetrieveRegionResponse, error) {
 	path := fmt.Sprintf("/store/regions/%v", regionId)
 	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
 	if err != nil {
@@ -21,11 +37,4 @@ func Retrieve(regionId string, config *medusa.Config) (*Region, error) {
 		return nil, err
 	}
 
-	respBody := new(RetrieveResponseBody)
-
-	if err := json.Unmarshal(body, respBody); err != nil {
-		return nil, err
-	}
-
-	return respBody.Region, nil
 }
