017ea217703363822f74f7586081cea564935c6b
harshmangalam <harshdev8218@gmail.com>
2022-10-22 13:41:37 +0530

    added doc comment

--- schema/customer.go
+++ schema/customer.go
@@ -5,19 +5,48 @@ import (
 )
 
 type Customer struct {
-	Email            string           `json:"email"`
-	Id               string           `json:"id"`
-	FirstName        string           `json:"first_name"`
-	LastName         string           `json:"last_name"`
-	BillingAddressId string           `json:"billing_address_id"`
-	BillingAddress   []*Address       `json:"billing_address"`
-	ShippingAddress  []*Address       `json:"shipping_address"`
-	Phone            string           `json:"phone"`
-	HasAccount       bool             `json:"has_account"`
-	Orders           []any            `json:"orders"`
-	Groups           []*CustomerGroup `json:"groups"`
-	CreatedAt        time.Time        `json:"created_at,omitempty"`
-	UpdatedAt        time.Time        `json:"updated_at,omitempty"`
-	DeletedAt        time.Time        `json:"deleted_at,omitempty"`
-	Metadata         any              `json:"metadata,omitempty"`
+	// The customer's email
+	Email string `json:"email"`
+
+	// The customer's ID
+	Id string `json:"id"`
+
+	// The customer's first name
+	FirstName string `json:"first_name"`
+
+	// The customer's last name
+	LastName string `json:"last_name"`
+
+	// The customer's billing address ID
+	BillingAddressId string `json:"billing_address_id"`
+
+	// An address.
+	BillingAddress []*Address `json:"billing_address"`
+
+	// Available if the relation shipping_addresses is expanded.
+	ShippingAddress []*Address `json:"shipping_address"`
+
+	// The customer's phone number
+	Phone string `json:"phone"`
+
+	// The customer's phone number
+	HasAccount bool `json:"has_account"`
+
+	// Available if the relation orders is expanded.
+	Orders []*Order `json:"orders"`
+
+	// The customer groups the customer belongs to. Available if the relation groups is expanded.
+	Groups []*CustomerGroup `json:"groups"`
+
+	// The date with timezone at which the resource was created.
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+
+	// The date with timezone at which the resource was updated.
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+
+	// The date with timezone at which the resource was deleted.
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+
+	// An optional key-value map with additional details
+	Metadata any `json:"metadata,omitempty"`
 }
