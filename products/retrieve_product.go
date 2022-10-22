5a1fd7940c53d5afe543a72d4c1054d5a38cdeaa
harshmangalam <harshdev8218@gmail.com>
2022-10-22 08:00:23 +0530

    added retrieve product respose

--- products/retrieve_product.go
+++ products/retrieve_product.go
@@ -6,8 +6,25 @@ import (
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
+
+type RetrieveProductData struct {
+	Product []*schema.Product `json:"product"`
+}
+
+type ListCollectionResponse struct {
+	// Success response
+	Data *RetrieveProductData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
 
 func Retrieve(id string, config *medusa.Config) ([]byte, error) {
 	path := fmt.Sprintf("/store/products/%v", id)
