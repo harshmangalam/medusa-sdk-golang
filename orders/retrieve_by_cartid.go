00e37509b3d5a462f78050979407bfebbe9d8697
harshmangalam <harshdev8218@gmail.com>
2022-10-22 11:30:48 +0530

    added retrieve by cart id response

---
+++ orders/retrieve_by_cartid.go
@@ -0,0 +1,78 @@
+package orders
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+type RetrieveByCartIdData struct {
+	Order *schema.Order `json:"order"`
+}
+
+type RetrieveByCartIdResponse struct {
+	// Success response
+	Data *RetrieveByCartIdData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+func RetrieveByCartId(cartId string, config *medusa.Config) (*RetrieveByCartIdResponse, error) {
+	path := fmt.Sprintf("/store/orders/cart/%v", cartId)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(RetrieveByCartIdResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(RetrieveByCartIdData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
+}
