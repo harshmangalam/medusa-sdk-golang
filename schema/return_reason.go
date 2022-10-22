2a648fcc27cc536e7ae4f08fef77afa9dfa003c5
harshmangalam <harshdev8218@gmail.com>
2022-10-22 09:08:32 +0530

    added doc comment

--- schema/return_reason.go
+++ schema/return_reason.go
@@ -2,16 +2,41 @@ package schema
 
 import "time"
 
+// A Reason for why a given product is returned. A Return Reason can be used on Return Items in order to indicate why a Line Item was returned.
+
 type ReturnReason struct {
-	Value                string         `json:"value"`
-	Label                string         `json:"Label"`
-	Id                   string         `json:"id"`
-	Description          string         `json:"description"`
-	ParentReturnReason   *ReturnReason  `json:"parent_return_reason"`
-	ParentReturnReasonId string         `json:"parent_return_reason_id"`
-	ReturnReasonChildren *ReturnReason  `json:"return_reason_children"`
-	CreatedAt            *time.Time     `json:"created_at"`
-	UpdatedAt            *time.Time     `json:"updated_at"`
-	DeletedAt            *time.Time     `json:"deleted_at"`
-	Metadata             map[string]any `json:"metadata"`
+	// The value to identify the reason by.
+	Value string `json:"value"`
+
+	// A text that can be displayed to the Customer as a reason.
+	Label string `json:"Label"`
+
+	// The cart's ID
+	Id string `json:"id"`
+
+	// A description of the Reason.
+	Description string `json:"description"`
+
+	// The ID of the parent reason.
+	ParentReturnReason *ReturnReason `json:"parent_return_reason"`
+
+	// A Reason for why a given product is returned. A Return Reason can be used on Return Items in order to indicate why a Line Item was returned.
+
+	ParentReturnReasonId string `json:"parent_return_reason_id"`
+
+	// A Reason for why a given product is returned. A Return Reason can be used on Return Items in order to indicate why a Line Item was returned.
+
+	ReturnReasonChildren *ReturnReason `json:"return_reason_children"`
+
+	// The date with timezone at which the resource was created.
+	CreatedAt *time.Time `json:"created_at"`
+
+	// The date with timezone at which the resource was updated.
+	UpdatedAt *time.Time `json:"updated_at"`
+
+	// The date with timezone at which the resource was deleted.
+	DeletedAt *time.Time `json:"deleted_at"`
+
+	// An optional key-value map with additional details
+	Metadata map[string]any `json:"metadata"`
 }
