bfd8a2269e7cd211477f52c0d5ad5a1795e57c1c
harshmangalam <harshdev8218@gmail.com>
2022-10-22 12:51:02 +0530

    added retrieve swap response

--- swaps/retrieve_by_cartid.go
+++ swaps/retrieve_by_cartid.go
@@ -1,16 +1,32 @@
 package swaps
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
-func RetrieveByCartId(cartId string, config *medusa.Config) (*Swap, error) {
+type RetrieveSwapData struct {
+	Swap *schema.Swap `json:"swap"`
+}
+
+type RetrieveSwapResponse struct {
+	// Success response
+	Data *RetrieveSwapData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+func RetrieveByCartId(cartId string, config *medusa.Config) (*RetrieveSwapResponse, error) {
 	path := fmt.Sprintf("/store/swaps/%v", cartId)
 	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
 	if err != nil {
@@ -21,11 +37,4 @@ func RetrieveByCartId(cartId string, config *medusa.Config) (*Swap, error) {
 		return nil, err
 	}
 
-	respBody := new(ResponseBody)
-
-	if err := json.Unmarshal(body, respBody); err != nil {
-		return nil, err
-	}
-
-	return respBody.Swap, nil
 }
