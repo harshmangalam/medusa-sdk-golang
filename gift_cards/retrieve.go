655c8f13a8db5f1ab25a26cdf88b20e0555140f5
harshmangalam <harshdev8218@gmail.com>
2022-10-22 12:30:51 +0530

    retrieve goft card

---
+++ gift_cards/retrieve.go
@@ -0,0 +1,82 @@
+package giftcards
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	medusa "github.com/harshmngalam/medusa-sdk-golang"
+	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
+	"github.com/harshmngalam/medusa-sdk-golang/utils"
+)
+
+type RetrieveGiftCardData struct {
+	GiftCard *schema.GiftCard `json:"gift_card"`
+}
+
+type RetrieveGiftCardResponse struct {
+	// Success response
+	Data *RetrieveGiftCardData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+type ResponseBody struct {
+	GiftCard *schema.GiftCard `json:"gift_card"`
+}
+
+func Retrieve(code string, config *medusa.Config) (*RetrieveGiftCardResponse, error) {
+	path := fmt.Sprintf("/store/gift-cards/%v", code)
+	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ParseResponseBody(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody := new(RetrieveGiftCardResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(RetrieveGiftCardData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
+}
