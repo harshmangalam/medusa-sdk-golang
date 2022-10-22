241b03ef16c3faddc73b18b9a8514ebd3ea1131c
harshmangalam <harshdev8218@gmail.com>
2022-10-22 18:06:21 +0530

    handle response and errors

--- customers/request_password_reset.go
+++ customers/request_password_reset.go
@@ -5,8 +5,20 @@ import (
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
+
+type RequestPasswordResetResponse struct {
+	// Success response
+	Data any
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
 
 type RequestPasswordReset struct {
 	Email string `json:"email"`
@@ -21,7 +33,7 @@ func (r *RequestPasswordReset) SetEmail(email string) *RequestPasswordReset {
 	return r
 }
 
-func (r *ResetPassword) Send(config *medusa.Config) ([]byte, error) {
+func (r *ResetPassword) Send(config *medusa.Config) (*RequestPasswordResetResponse, error) {
 	path := "/store/customers/password-token"
 	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(r).Send(config)
 	if err != nil {
@@ -32,5 +44,37 @@ func (r *ResetPassword) Send(config *medusa.Config) ([]byte, error) {
 		return nil, err
 	}
 
-	return body, nil
+	respBody := new(RequestPasswordResetResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respBody.Data = nil
+
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
+	}
+
+	return respBody, nil
 }
