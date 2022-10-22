20456d861e1d89b9926057e960f782e947bd21ee
harshmangalam <harshdev8218@gmail.com>
2022-10-22 07:56:12 +0530

    added product schema

--- schema/product.go
+++ schema/product.go
@@ -3,9 +3,7 @@ package schema
 import (
 	"time"
 
-	"github.com/harshmngalam/medusa-sdk-golang/collections"
 	"github.com/harshmngalam/medusa-sdk-golang/common"
-	"github.com/harshmngalam/medusa-sdk-golang/products"
 )
 
 type ProductStatus string
@@ -65,37 +63,37 @@ type Tag struct {
 }
 
 type Product struct {
-	Title         string                     `json:"title"`
-	ProfileId     string                     `json:"profile_id"`
-	Id            string                     `json:"id"`
-	Subtitle      string                     `json:"subtitle"`
-	Description   string                     `json:"description"`
-	Handle        string                     `json:"handle"`
-	IsGiftcard    bool                       `json:"is_giftcard"`
-	Status        ProductStatus              `json:"status"`
-	Images        []*common.Image            `json:"images"`
-	Thumbnail     string                     `json:"thumbnail"`
-	Options       []*ProductOption           `json:"options"`
-	Variants      []*products.ProductVariant `json:"variants"`
-	Profile       *ShippingProfile           `json:"profile"`
-	Weight        int                        `json:"weight"`
-	Height        int                        `json:"height"`
-	Width         int                        `json:"width"`
-	Length        int                        `json:"length"`
+	Title         string            `json:"title"`
+	ProfileId     string            `json:"profile_id"`
+	Id            string            `json:"id"`
+	Subtitle      string            `json:"subtitle"`
+	Description   string            `json:"description"`
+	Handle        string            `json:"handle"`
+	IsGiftcard    bool              `json:"is_giftcard"`
+	Status        ProductStatus     `json:"status"`
+	Images        []*common.Image   `json:"images"`
+	Thumbnail     string            `json:"thumbnail"`
+	Options       []*ProductOption  `json:"options"`
+	Variants      []*ProductVariant `json:"variants"`
+	Profile       *ShippingProfile  `json:"profile"`
+	Weight        int               `json:"weight"`
+	Height        int               `json:"height"`
+	Width         int               `json:"width"`
+	Length        int               `json:"length"`
 	HsCode        string
-	OriginCountry string                  `json:"origin_country"`
-	MidCode       string                  `json:"mid_code"`
-	Material      string                  `json:"material"`
-	CollectionId  string                  `json:"collection_id"`
-	Collection    *collections.Collection `json:"collection"`
-	TypeId        string                  `json:"type_id"`
-	Type          *ProductType            `json:"type"`
-	Tags          []*Tag                  `json:"tags"`
-	Discountable  bool                    `json:"discountable"`
-	ExternalId    string                  `json:"external_id"`
-	SalesChannels string                  `json:"sales_channels"`
-	CreatedAt     *time.Time              `json:"created_at"`
-	UpdatedAt     *time.Time              `json:"updated_at"`
-	DeletedAt     *time.Time              `json:"deleted_at"`
-	Metadata      map[string]any          `json:"metadata"`
+	OriginCountry string         `json:"origin_country"`
+	MidCode       string         `json:"mid_code"`
+	Material      string         `json:"material"`
+	CollectionId  string         `json:"collection_id"`
+	Collection    *Collection    `json:"collection"`
+	TypeId        string         `json:"type_id"`
+	Type          *ProductType   `json:"type"`
+	Tags          []*Tag         `json:"tags"`
+	Discountable  bool           `json:"discountable"`
+	ExternalId    string         `json:"external_id"`
+	SalesChannels string         `json:"sales_channels"`
+	CreatedAt     *time.Time     `json:"created_at"`
+	UpdatedAt     *time.Time     `json:"updated_at"`
+	DeletedAt     *time.Time     `json:"deleted_at"`
+	Metadata      map[string]any `json:"metadata"`
 }
