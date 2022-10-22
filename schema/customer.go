10c65b705f269c9b6445237a4daf72e72481925d
harshmangalam <harshdev8218@gmail.com>
2022-10-22 13:22:15 +0530

    added customer schema

---
+++ schema/customer.go
@@ -0,0 +1,23 @@
+package schema
+
+import (
+	"time"
+)
+
+type Customer struct {
+	Email            string           `json:"email"`
+	Id               string           `json:"id"`
+	FirstName        string           `json:"first_name"`
+	LastName         string           `json:"last_name"`
+	BillingAddressId string           `json:"billing_address_id"`
+	BillingAddress   []*Address       `json:"billing_address"`
+	ShippingAddress  []*Address       `json:"shipping_address"`
+	Phone            string           `json:"phone"`
+	HasAccount       bool             `json:"has_account"`
+	Orders           []any            `json:"orders"`
+	Groups           []*CustomerGroup `json:"groups"`
+	CreatedAt        time.Time        `json:"created_at,omitempty"`
+	UpdatedAt        time.Time        `json:"updated_at,omitempty"`
+	DeletedAt        time.Time        `json:"deleted_at,omitempty"`
+	Metadata         any              `json:"metadata,omitempty"`
+}
