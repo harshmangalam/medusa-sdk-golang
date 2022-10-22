6a18a6fbbdd2191461d3d365a64e972351c6e0fa
harshmangalam <harshdev8218@gmail.com>
2022-10-22 12:43:41 +0530

    retrieve response and errors

--- order_edits/retrieve.go
+++ order_edits/retrieve.go
@@ -7,10 +7,28 @@ import (
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
 
-func Retrieve(id string, config *medusa.Config) (*OrderEdit, error) {
+type RetrieveData struct {
+	// Gift Cards are redeemable and represent a value that can be used towards the payment of an Order.
+	OrderEdit *schema.OrderEdit `json:"order_edit"`
+}
+
+type RetrieveResponse struct {
+	// Success response
+	Data *RetrieveData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
+
+func Retrieve(id string, config *medusa.Config) (*RetrieveResponse, error) {
 	path := fmt.Sprintf("/store/order-edits/%v", id)
 	resp, err := request.NewRequest().SetMethod(http.MethodGet).SetPath(path).Send(config)
 	if err != nil {
@@ -21,11 +39,41 @@ func Retrieve(id string, config *medusa.Config) (*OrderEdit, error) {
 		return nil, err
 	}
 
-	respBody := new(RetrieveOrderEditResponse)
+	respBody := new(RetrieveResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(RetrieveData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
 
-	if err := json.Unmarshal(body, respBody); err != nil {
-		return nil, err
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
 	}
 
-	return respBody.OrderEdit, nil
+	return respBody, nil
 }
