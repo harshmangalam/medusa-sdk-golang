1d1848fc545dfae67b206d0c7236875b1bb694e2
harshmangalam <harshdev8218@gmail.com>
2022-10-22 11:42:00 +0530

    added gift card schema

---
+++ schema/gift_card.go
@@ -0,0 +1,20 @@
+package schema
+
+import "time"
+
+type GiftCard struct {
+	Code       string         `json:"code"`
+	Value      int            `json:"value"`
+	Balance    int            `json:"balance"`
+	RegionId   string         `json:"region_id"`
+	Id         string         `json:"id,omitempty"`
+	Region     string         `json:"region,omitempty"`
+	OrderId    string         `json:"order_id,omitempty"`
+	Order      *Order         `json:"order,omitempty"`
+	IsDisabled bool           `json:"is_disabled,omitempty"`
+	EndsAt     *time.Time     `json:"ends_at,omitempty"`
+	CreatedAt  *time.Time     `json:"created_at,omitempty"`
+	UpdatedAt  *time.Time     `json:"updated_at,omitempty"`
+	DeletedAt  *time.Time     `json:"deleted_at,omitempty"`
+	Metadata   map[string]any `json:"metadata,omitempty"`
+}
