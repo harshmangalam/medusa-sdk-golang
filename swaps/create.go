b02d8a3aa161693a19d9364d5e63e0762c87fea6
harshmangalam <harshdev8218@gmail.com>
2022-10-22 12:48:29 +0530

    added swap create schema and response

--- swaps/create.go
+++ swaps/create.go
@@ -6,8 +6,25 @@ import (
 
 	medusa "github.com/harshmngalam/medusa-sdk-golang"
 	"github.com/harshmngalam/medusa-sdk-golang/request"
+	"github.com/harshmngalam/medusa-sdk-golang/response"
+	"github.com/harshmngalam/medusa-sdk-golang/schema"
 	"github.com/harshmngalam/medusa-sdk-golang/utils"
 )
+
+type CreateSwapData struct {
+	Swap *schema.Swap `json:"swap"`
+}
+
+type CreateSwapResponse struct {
+	// Success response
+	Data *CreateSwapData
+
+	// Error response
+	Error *response.Error
+
+	// Errors in case of multiple errors
+	Errors *response.Errors
+}
 
 type CreateSwap struct {
 	OrderId              string `json:"order_id"`
@@ -38,7 +55,7 @@ func (c *CreateSwap) SetReturnShippingOption(shippingOpions string) *CreateSwap 
 	return c
 }
 
-func (c *CreateSwap) Create(config *medusa.Config) (*Swap, error) {
+func (c *CreateSwap) Create(config *medusa.Config) (*CreateSwapResponse, error) {
 	path := "/store/swaps"
 	resp, err := request.NewRequest().SetMethod(http.MethodPost).SetPath(path).SetData(c).Send(config)
 	if err != nil {
@@ -48,11 +65,42 @@ func (c *CreateSwap) Create(config *medusa.Config) (*Swap, error) {
 	if err != nil {
 		return nil, err
 	}
-	respBody := new(ResponseBody)
+	respBody := new(CreateSwapResponse)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		respData := new(CreateSwapData)
+		if err := json.Unmarshal(body, respData); err != nil {
+			return nil, err
+		}
+		respBody.Data = respData
 
-	if err := json.Unmarshal(body, respBody); err != nil {
-		return nil, err
+	case http.StatusUnauthorized:
+		respErr := utils.UnauthorizeError()
+		respBody.Error = respErr
+
+	case http.StatusBadRequest:
+		respErrors, err := utils.ParseErrors(body)
+		if err != nil {
+			return nil, err
+		}
+		if len(respErrors.Errors) == 0 {
+			respError, err := utils.ParseError(body)
+			if err != nil {
+				return nil, err
+			}
+			respBody.Error = respError
+		} else {
+			respBody.Errors = respErrors
+		}
+
+	default:
+		respErr, err := utils.ParseError(body)
+		if err != nil {
+			return nil, err
+		}
+		respBody.Error = respErr
 	}
 
-	return respBody.Swap, nil
+	return respBody, nil
+
 }
